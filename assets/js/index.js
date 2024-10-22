be635323ae971cbbe638049d469805e8a9f294ae
MamatovaElnoza <mamatovaelnoza@gmail.com>
2024-10-22 14:46:01 +0300

    enhanced

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,4 +5,7 @@ const themeToggler = document.querySelector('.theme-toggler');
 //change theme
 themeToggler.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('i:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('i:nth-child(2)').classList.toggle('active');
 })
